aa3e2a504eea68d875b8a018f0f296171fca0461
Josh Ourisman <josh@joshourisman.com>
2020-07-28 16:20:42 -0400

    Converted page to Next.js.

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>How many fucks does Elon Musk give?</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-60956755-1', 'auto');
+    ga('send', 'pageview');`,
+          }}
+        />
+      </Head>
+      <h1 style={{ fontSize: '20vw', textAlign: 'center' }}>ZERO FUCKS</h1>
+    </div>
+  );
+}
